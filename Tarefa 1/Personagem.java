d741961076ab84e32b1f545794492627c9d51e7d
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-08-25 22:55:45 -0300

    Criação da classe "Herói"

--- Tarefa 1/Personagem.java
+++ Tarefa 1/Personagem.java
@@ -5,15 +5,16 @@ public class Personagem{
     int pontosDeVida;
     int forca;
 
-    //Métodos
-    public Personagem(String name, int HP, int power){ //Construtor
+    //Construtor
+    public Personagem(String name, int HP, int power){
         this.nome = name;
         this.pontosDeVida = HP;
         this.forca = power;
     }
 
+    //Métodos
     public void receberDano(int DMG){
-        this.pontosDeVida = this.pontosDeVida - DMG;
+        this.pontosDeVida -= DMG;
     }
 
     public String exibirStatus(){
@@ -23,5 +24,29 @@ public class Personagem{
     public void atacar(Personagem alvo){
         
     }
+}
 
+
+class Heroi extends Personagem{
+
+    //Atributos
+    int nivel;
+    int experiencia;
+
+    //Construtor
+    public Heroi(String name, int HP, int power, int lvl, int XP){
+        super(name, HP, power);
+        this.nivel = lvl;
+        this.experiencia = XP;
+    }
+
+    //Métodos
+    public void ganharExperiencia(int new_XP){
+        this.experiencia += new_XP;
+    }
+
+    @Override
+    public String exibirStatus(){
+        return String.format("Nome do personagem: %s\nQuantidade de vida: %d\nNível do personagem: %d\nQuantidade de experiência: %d\n", nome, pontosDeVida, nivel, experiencia);
+    }
 }
