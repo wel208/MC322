8b286f0f545d3265a0e4dca75394662ed20a1c07
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-09-11 03:07:21 -0300

    Algumas mínimas coisas na Main

--- tarefa2/src/Main.java
+++ tarefa2/src/Main.java
@@ -1,11 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
-        Heroi heroi = Utilidades.criarHeroi();
-
-        heroi.exibirStatus();
+        Random r = new Random();
 
         boolean heroiMorto = false;
 
-        
+        Heroi heroi = Utilidades.criarHeroi();
+        heroi.exibirStatus();
+
+        ConstrutorDeCenario.gerarFases(r.nextInt(2, 10));
     }
 }
