f4152dc64f86a7d86c2b18fe8b8af8b462ea4789
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-09-16 21:49:56 -0300

    Remoção de métodos em 'AcaoDeCombate'. Deixado apenas o método 'executar'

--- tarefa3/src/AcaoDeCombate.java
+++ tarefa3/src/AcaoDeCombate.java
@@ -1,6 +1,3 @@
 public interface AcaoDeCombate {
-    void atacar(Combatente usuario, Combatente alvo);
-    void usarHabilidadeEspecial(Combatente usuario, Combatente alvo);
-    void mover(Combatente usuario, Combatente alvo);
-    void tomarDecisao(Combatente usuario, Combatente alvo);
+    void executar(Combatente usuario, Combatente alvo);
 }
