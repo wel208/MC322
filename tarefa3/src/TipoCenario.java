2b2f8e6f09a893a74c52017f84b748a316d5a407
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-09-16 21:20:50 -0300

    Criação do enum 'TipoCenario'

---
+++ tarefa3/src/TipoCenario.java
@@ -0,0 +1,19 @@
+public enum TipoCenario {
+    //Constantes
+    CASTELO ("VAMOS! O castelo esta sendo invadido!"),
+    ACAMPAMENTO ("Tenha cuidado. Ha monstros la fora."),
+    VILAREJO ("Um vilarejo abandonado. Monstros tomaram conta deste lugar.");
+
+    //Atributos
+    private final String descricao;
+
+    //Construtor
+    TipoCenario(String descricao){
+        this.descricao = descricao;
+    }
+
+    //Métodos
+    public void descreverCenario(){
+        System.out.println(descricao);
+    }
+}
