f38ee95043bc446b06fbae51f5fc423b50093a7c
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-09-17 14:14:39 -0300

    Revert "?"
    This reverts commit 2188b54acec0e8ef2a52ec373505d7ef980054ee.

--- tarefa3/src/AtaqueComum.java
+++ tarefa3/src/AtaqueComum.java
@@ -4,7 +4,7 @@ public class AtaqueComum implements AcaoDeCombate{
     public void executar(Combatente usuario, Combatente alvo){
         if (usuario.getArma().getTipo() == "Longo Alcance"){
 
-            
+
         }
         else{
 
