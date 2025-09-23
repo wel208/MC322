8f4fef662e08c532b8bb79cb82e599f9e8d4ff5e
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-09-22 21:58:37 -0300

    Criação da classe InputManager

---
+++ tarefa4/src/com/rpg/game/InputManager.java
@@ -0,0 +1,25 @@
+package com.rpg.game;
+
+public class InputManager {
+    
+    //Métodos
+    public static int lerInteiro(String mensagem, int min, int max){
+
+    }
+
+    public static String lerString(String mensagem){
+
+    }
+
+    public static boolean lerSimNao(String mensagem){
+
+    }
+
+    public static void esperarEnter(String mensagem){
+
+    }
+
+    public static void fecharScanner(){
+        
+    }
+}
