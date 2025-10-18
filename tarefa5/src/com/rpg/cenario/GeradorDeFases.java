9887cd5bfb1a09c9803b7801eb52c0c27a15d802
Welinson Miguel Dantas <welinmiguel@gmail.com>
2025-10-18 16:08:27 -0300

    Delete tarefa5 directory

--- tarefa5/src/com/rpg/cenario/GeradorDeFases.java
+++
@@ -1,6 +0,0 @@
-package com.rpg.cenario;
-
-import java.util.List;
-public interface GeradorDeFases {
-    List<Fase> gerar(int quantidadeDeFases, Dificuldade dificuldade);
-}
--- tarefa5/src/com/rpg/eventos/Evento.java
+++
@@ -1,7 +0,0 @@
-package com.rpg.eventos;
-
-import com.rpg.combate.*;
-public interface Evento {
-    boolean verificarGatilho(Combatente heroi, Combatente monstro);
-    void executar(Combatente alvo);
-}
